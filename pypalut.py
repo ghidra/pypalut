45f328a7f6f7121b5691e71e74b9722f3ce8d0e0
ghidra <ghidra@gmail.com>
2015-02-02 14:32:36 -0500

    working, needs new algo

--- pypalut.py
+++ pypalut.py
@@ -8,12 +8,14 @@ debug=True
 #colors_all=[]
 
 table=[]#vanilla table generated based on span
+table_rgb=[]
 table_hls=[]#the hls space verion
 table_hsv=[]#the hsv space version
 table_palette=[]#the palette being fitted
 table_distance=[]#the distance of the palette from the table for comparrison
 
 span = 8 # this is the cube root of the table ie 8*8*8
+max_samples = 50000
 #similar_threshold = 1.0
 
 def load_image(im):
@@ -23,27 +25,16 @@ def load_image(im):
   except:
     return "error"
 
-def color_distance(c1,c2):
+def color_distance_rgb(c1,c2):
+    #rgb distance
     rm = 0.5*(c1[0]+c2[0])
-    #d = sum( (2+rm,4,3-rm)*( (c1[0]-c2[0])+(c1[1]-c2[1])+(c1[2]-c2[2]))**2 )**0.5
+    #d = sum( (2+rm,4,3-rm)*( c1-c2 )**2 )**0.5
 
     a = 2.0+rm+4.0+3.0-rm
     b = (c1[0]-c2[0])+(c1[1]-c2[1])+(c1[2]-c2[2])
     d = (a*b**2.0)**0.5
 
     return d
-
-'''def check_against_colors(c):
-    smallest_distance = 1000.0
-    nearest_match = -1
-    count = 0
-    for oc in colors_all:
-        distance = color_distance(c,oc)
-        if distance<smallest_distance:
-            smallest_distance = distance
-            nearest_match = count
-        count += 1
-    return [smallest_distance, nearest_match]'''
 
 def build_vanilla_table():
     global span
@@ -60,6 +51,7 @@ def build_vanilla_table():
         b = int(math.floor((plane/(span-1.0))*255.0))
 
         table.append((r,g,b))
+        table_rgb.append( (r/255.0,g/255.0,b/255.0) )
         table_hls.append(colorsys.rgb_to_hls(r/255.0,g/255.0,b/255.0))
         table_hsv.append(colorsys.rgb_to_hsv(r/255.0,g/255.0,b/255.0))
 
@@ -84,7 +76,7 @@ def display_table(file=""):
 
         x = int((plane*span)+column)
         y = int(row)
-        im.paste(i,(x,y,x+1,y+1))
+        im.paste(i,(x,y,int(x+1),int(y+1)))
         count+=1
     #im.show()
     if file!="":
@@ -99,8 +91,9 @@ def main_DEBUG(kwargs):
 
 def main(kwargs):
 
-    global colors_all;
-    global debug;
+    global colors_all
+    global debug
+    global max_samples
 
     build_vanilla_table()
 
@@ -108,24 +101,29 @@ def main(kwargs):
 
     if my_im != "error":
         w,h = my_im.size
+        fuzzy = 2 #int(math.floor( max(1,( (w*h)/max_samples ) ) ) )
+        #fuzzy = (w*h)-max_samples
         #count=0
         if debug:
             print "image width:"+str(w)
             print "image height:"+str(h)
             print "number of pixels:"+str(w*h)
+            print "fuzzy:"+str(fuzzy)
+            print "fuzzy width:"+str(math.floor(w/fuzzy))
+            print "fuzzy width:"+str(math.floor(h/fuzzy))
         l = my_im.load()
-        for x in range(0,w-1):
-            for y in range(0,h-1):
+        for x in range(0,w-1,fuzzy):
+            for y in range(0,h-1,fuzzy):
                 c = l[x,y]
                 #print c
-                #c = (c[0]/255.0,c[1]/255.0,c[2]/255.0) #convert to 0-1
-                chls = colorsys.rgb_to_hls(c[0]/255.0,c[1]/255.0,c[2]/255.0)
+                c_rgb = (c[0]/255.0,c[1]/255.0,c[2]/255.0) #convert to 0-1
+                #chls = colorsys.rgb_to_hls(c[0]/255.0,c[1]/255.0,c[2]/255.0)
                 #chsv = colorsys.rgb_to_hsv(c[0],c[1],c[2])
 
                 #loop against the vanilla palette
                 count = 0
-                for tc in table_hls:
-                    distance = color_distance(chls,tc)
+                for tc in table_rgb:
+                    distance = color_distance_rgb(c_rgb,tc)
                     #print distance
                     if(distance < table_distance[count]):
                         table_palette[count]=c
